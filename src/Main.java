25e474f2c4798a16049f197051f1e9a7d8aec9f9
subair7 <subairmusthafa1991@gmail.com>
2024-03-09 16:13:30 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main implements Sample {
+    public static void main(String[] args) {
+     Stack myStack=new Stack(4);
+
+     myStack.getTop();
+     myStack.getHeight();
+
+     myStack.printStack();
+
+    }
+
+    @Override
+    public void darw() {
+        System.out.println("i = ");
+    }
+}
