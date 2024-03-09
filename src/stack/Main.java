528d39bd02ac20f08ad0d70f53434f58bcaa298b
subair7 <subairmusthafa1991@gmail.com>
2024-03-09 18:37:34 +0530

    Added Stack Push

--- src/stack/Main.java
+++ src/stack/Main.java
@@ -1,20 +1,16 @@
+package stack;
+
 import stack.Stack;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main implements Sample {
+public class Main {
     public static void main(String[] args) {
      Stack myStack=new Stack(4);
 
-     myStack.getTop();
-     myStack.getHeight();
-
+    myStack.push(2);
      myStack.printStack();
 
     }
 
-    @Override
-    public void darw() {
-        System.out.println("i = ");
-    }
 }
--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -30,4 +30,15 @@ public class Stack {
     public void getHeight(){
         System.out.println("Height "+height);
     }
+    public  void push(int value){
+        Node newNode=new Node(value);
+        if (height==0){
+            top=newNode;
+        }else{
+            newNode.next=top;
+            top=newNode;
+        }
+        height++;
+
+    }
 }
